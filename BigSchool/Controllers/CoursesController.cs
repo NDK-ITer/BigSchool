82633f988cf4f6e2fa9bc21774acfc1d1e78e1ef
NDK-ITer <116534879+NDK-ITer@users.noreply.github.com>
2023-03-10 16:05:51 +0700

    buoi 1

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,52 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public CoursesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new CoursesViewModel
+            {
+                Categoies = dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CoursesViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                viewModel.Categoies = dbContext.Categories.ToList();
+                return View("Create",viewModel);
+            }
+            var courses = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CategoryId = viewModel.Category,
+                Place = viewModel.Place
+            };
+            dbContext.Course.Add(courses);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ BigSchool/ViewModels/CoursesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool.ViewModels
+{
+    public class CoursesViewModel
+    {
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        public string Place { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [FutureDate]
+        public string Date { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        [ValidTime]
+        public string Time { get; set; }
+        [Required(ErrorMessage = "Bắt buộc nhập")]
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categoies { get; set; }
+        public DateTime GetDateTime() 
+        { 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time)); 
+        }  
+    }
+}
